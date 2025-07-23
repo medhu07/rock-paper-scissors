c446992119bc82e96efdb7f7c587392f3cda8164
medhu07 <47105060+medhu07@users.noreply.github.com>
2025-07-23 13:51:10 +0530

    Add files via upload

---
+++ rock-paper-scissors.js
@@ -0,0 +1,50 @@
+let result = '';
+      let userChoice = '';
+      let computerChoice = '';
+
+      let score = JSON.parse(localStorage.getItem('score')) || {
+        wins: 0,
+        losses: 0,
+        ties: 0
+      };
+
+      function updateResultElement() {
+        document.querySelector('.js-result').innerHTML = result;
+      }
+
+      function updateMovesElement() {
+        document.querySelector('.js-moves').innerHTML = `YOU PICKED
+          <img src="${userChoice}-emoji.png" class="move-icon">
+          <img src="${computerChoice}-emoji.png" class="move-icon">
+          COMPUTER PICKED`;
+      }
+
+      function updateScoreElement() {
+        document.querySelector('.js-score').innerHTML = 
+          `SCORE: ${score.wins} wins, ${score.losses} losses, ${score.ties} ties`;
+      }
+
+      function computermove() {
+        const randomNumber = Math.random();
+        if (randomNumber < 0.33) {
+          computerChoice = 'rock';
+        } else if (randomNumber < 0.66) {
+          computerChoice = 'paper';
+        } else {
+          computerChoice = 'scissors';
+        }
+      }
+
+      function updateScore(result) {
+        if (result === 'You win') {
+          score.wins++;
+        } else if (result === 'You lose') {
+          score.losses++;
+        } else {
+          score.ties++;
+        }
+        localStorage.setItem('score', JSON.stringify(score));
+      }
+
+      // Show score on first load
+      updateScoreElement();
